ba789ef7daec0d885544fd8f45e1231bb8203403
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-02-10 17:54:17 +0100

    adds controller functions to router

--- backend/routers/todosRouter.js
+++ backend/routers/todosRouter.js
@@ -1,6 +1,22 @@
 import express from 'express'
-import { addSingleTodo } from '../controllers/todosController.js'
+import {
+    addSingleTodo,
+    getSingleTodo,
+    getAllTodos,
+    updateSingleTodo,
+    deleteSingleTodo,
+    deleteAllTodos,
+} from '../controllers/todosController.js'
 
 export const todosRouter = express.Router()
 
-todosRouter.route('/').post(addSingleTodo)
+todosRouter
+    .route('/')
+    .post(addSingleTodo)
+    .get(getAllTodos)
+    .delete(deleteAllTodos)
+todosRouter
+    .route('/:id')
+    .get(getSingleTodo)
+    .patch(updateSingleTodo)
+    .delete(deleteSingleTodo)
